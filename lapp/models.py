ed7ad710e5211c39e23a6d1787b47d3d739bd63c
taspee <tfaspee@miuandes.cl>
2024-12-03 19:39:47 -0300

    agregado de controller model

--- lapp/models.py
+++ lapp/models.py
@@ -17,4 +17,9 @@ class Casillero(models.Model):
     def __str__(self):
         return f"Casillero {self.id} de {self.usuario.name}"
 
+class Contoller(models.Model):
+    # Atributos para el modelo Casillero
+    casillero = models.ForeignKey(Casillero, on_delete=models.CASCADE)  # Relación con el modelo Usuario
 
+    def __str__(self):
+        return self.name
