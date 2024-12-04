4811f2b72248a65535c8e9bc49ace0896ca9059c
taspee <tfaspee@miuandes.cl>
2024-12-04 09:42:56 -0300

    admin

--- lapp/admin.py
+++ lapp/admin.py
@@ -1,6 +1,7 @@
 # lockers/admin.py
 from django.contrib import admin
-from .models import Usuario, Casillero
+from .models import Usuario, Casillero, Contoller
 
 admin.site.register(Usuario)
 admin.site.register(Casillero)
+admin.site.register(Controller)
